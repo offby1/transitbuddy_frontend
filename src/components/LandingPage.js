ae1e93e3e2887c383684d5e1e20f0a365b2f38c8
Eric Hanchrow <eric.hanchrow@gmail.com>
2023-06-11 12:31:34 -0700

    Use relative paths to image files and whatnot.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 
 //images
-import Banner1 from '/home/richarda/Bootcamp/project_MTA/frontend_app/src/Banner1.jpg';
+import Banner1 from '../Banner1.jpg';
 function LandingPage() {
   return (
     <img className = "homebackground" src = {Banner1}/>
